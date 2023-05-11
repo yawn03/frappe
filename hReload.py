fe5685854ddb8256736d137229bdc05d867dbe78
April <maprilbear@gmail.com>
2023-05-11 13:13:55 -0500

    Fixed hash comparison bug migrated to scheduled events

--- hReload.py
+++ hReload.py
@@ -4,6 +4,7 @@ import signal
 import time
 from dotenv import dotenv_values
 import sys
+import sched
 
 def get_commit_hash(user, repo, br):
     url = f"https://api.github.com/repos/{user}/{repo}/branches/{br}"
@@ -13,7 +14,7 @@ def get_commit_hash(user, repo, br):
     if (response.status_code == 200):
         lHash = response.json()["commit"]["sha"]
         msg = response.json()["commit"]["commit"]["message"]
-        return [lHash, msg]
+        return str(lHash)
     else:
         print(f"Failed to retrieve latest commit hash. Code: {response.status_code}")
         return None
@@ -34,7 +35,29 @@ shaPr = get_commit_hash(user, repo, branch)
 
 pHandle = startBot()
 
-while (True):
+def check_for_new_commit(scheduler, pHandle, shaPr):
+    print("oh boy! time to check github!")
+    scheduler.enter(10,1,check_for_new_commit, (scheduler, pHandle, shaPr,))
+    commit = get_commit_hash(user, repo, branch)
+    sha = commit
+    # new commit
+    if (shaPr != sha):
+        print(f"new commit on '{branch}': {sha[-4:]}; '{commit[1]}' - reloading bot")
+        pHandle.send_signal(signal.SIGTERM)
+        pHandle.wait()
+        pHandle = startBot()
+        shaPr = sha
+    else:
+        print("no new commit D:")
+
+
+my_scheduler = sched.scheduler(time.time, time.sleep)
+my_scheduler.enter(float(env_vars["COMMIT_CHECK_INTERVAL"]), 1, check_for_new_commit, (my_scheduler, pHandle, shaPr))
+print("running scheduled tasks!")
+my_scheduler.run()
+print("going to sleep now :)")
+
+#while (True):
     # this code does not work and i am not sure why rn
     # could be useful later
     # bot is frozen
@@ -45,15 +68,3 @@ while (True):
 #        pHandle = startBot()
     
     # Check github every 60 seconds
-    if ((time.time() % 60) == 0):
-        commit = get_commit_hash(user, repo, branch)
-        sha = commit[0]
-        # new commit
-        if (shaPr != sha):
-            print(f"new commit on '{branch}': {sha[-4:]}; '{commit[1]}' - reloading bot")
-            pHandle.send_signal(signal.SIGTERM)
-            pHandle.wait()
-            pHandle = startBot()
-            shaPr = sha
-        else:
-            print("no new commit D:")
