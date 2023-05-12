a045289f5b26f82ade6e76a3991d7e0a8daf042d
Ayan Chaudhry <ayanchaudhry1@gmail.com>
2023-05-11 22:17:07 -0500

    attempt at pulling code and resetting bot

--- webhookReload.py
+++ webhookReload.py
@@ -1,19 +1,33 @@
+from subprocess import Popen
+
 from dotenv import dotenv_values
 from flask import Flask, request
+
+import hReload
 
 app = Flask(__name__)
 
 env_vars = dotenv_values(".env")
-user = env_vars["STAGING_USER"]
-repo = env_vars["STAGING_REPO"]
 branch = env_vars["STAGING_BRANCH"]
-token = env_vars["PERSONAL_GITHUB_TOKEN"]
+pHandle: Popen = hReload.startBot()
 
 
 @app.route("/update", methods=["POST"])
 def update():
-    print(request.json)
+    # print(request.json)
+    try:
+        if request.json["ref"] == ("refs/heads/" + branch):
+            print("good update")
+            global pHandle
+            pHandle = hReload.reset_bot(pHandle)
+    except:
+        print("not a push or not correct branch")
+        return "received!"
+
+    return "received!"
+
 
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5555, debug=True)
+
